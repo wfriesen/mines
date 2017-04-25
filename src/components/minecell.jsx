0f5ecfd52ed40a6d28e26e4814a7fb7f6f1799ce
William Friesen <wfriesen@gmail.com>
2017-04-25 12:28:46 +1000

    Refactored minecell classes

--- src/components/minecell.jsx
+++ src/components/minecell.jsx
@@ -7,9 +7,12 @@ class MineCell extends Component {
   }
 
   render() {
+    const minecellClass = 'minecell-' + this.props.surrounding_mines;
+    const flagClass = this.props.flag ? 'flagged': '';
+    const coveredClass = this.props.covered ? 'covered' : 'uncovered';
     return (
       <td
-        className={`${this.props.covered ? 'covered' : 'uncovered'} minecell minecell-${this.props.surrounding_mines} ${this.props.flag ? 'flagged': ''}`}
+        className={`minecell ${coveredClass} ${minecellClass} ${flagClass}`}
         onContextMenu={(e) => this.props.onContextMenu(e, this.props.row, this.props.column)}
         onClick={() => this.props.onClick(this.props.row, this.props.column)}>
         {this.props.surrounding_mines}
