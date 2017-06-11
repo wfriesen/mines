5db5b8982d2d53824c1675eb444a02ec190fd081
William Friesen <wfriesen@gmail.com>
2017-06-11 13:53:49 +1000

    Refactored to allow for restarting game
    Moved the field state into App, rather than the field itself

--- src/components/minefield.jsx
+++ src/components/minefield.jsx
@@ -5,114 +5,23 @@ class MineField extends Component {
 
   constructor(props) {
     super(props);
-
-    const field = this.generateField();
-    this.state = {field}
-  }
-
-  generateField() {
-    // Generate initial, empty field
-    var field = [];
-    for (var i=0; i<this.props.height; i++) {
-      var cells = []
-      for (var j=0; j<this.props.width; j++) {
-        cells.push({
-          covered: true,
-          flag: false,
-          surrounding_mines: 0
-        })
-      }
-      field.push(cells);
-    }
-
-    // Add mines
-    for (var i=0; i<this.props.mines; i++) {
-      var row = Math.floor(Math.random() * this.props.height);
-      var column = Math.floor(Math.random() * this.props.width);
-      if ( field[row][column].surrounding_mines == 0) {
-        field[row][column].surrounding_mines = 9;
-      } else {
-        i--;
-      }
-    }
-
-    // Set surrounding mine counts
-    for (var i=0; i<this.props.height; i++) {
-      for (var j=0; j<this.props.width; j++) {
-        if ( field[i][j].surrounding_mines == 9 ) {
-          continue;
-        }
-
-        this.getSurroundingCells(i, j).forEach((item) => {
-          if (field[item[0]][item[1]].surrounding_mines == 9) {
-            field[i][j].surrounding_mines++;
-          }
-        });
-      }
-    }
-
-    return field;
-  }
-
-  getSurroundingCells(row, column) {
-    const cells = [];
-    [
-      [row-1, column-1],
-      [row-1, column],
-      [row-1, column+1],
-      [row, column-1],
-      [row, column+1],
-      [row+1, column-1],
-      [row+1, column],
-      [row+1, column+1]
-    ].forEach((cell) => {
-      if (cell[0] >= 0 && cell[1] >= 0 && cell[0] < this.props.height && cell[1] < this.props.width) {
-        cells.push([cell[0], cell[1]]);
-      }
-    });
-    return cells;
-  }
-
-
-  uncoverCell (row, column) {
-    const field = this.state.field;
-    if ( field[row][column].covered && !field[row][column].flag ) {
-      field[row][column].covered = false;
-      if ( field[row][column].surrounding_mines == 0 ) {
-        this.getSurroundingCells(row, column).forEach((cell) => {
-          if ( field[cell[0]][cell[1]].covered ) {
-            this.uncoverCell(cell[0], cell[1]);
-          }
-        });
-      }
-      this.setState({field});
-    }
-  }
-
-  flagCell (e, row, column) {
-    e.preventDefault();
-    const field = this.state.field;
-    if ( field[row][column].covered ) {
-      field[row][column].flag = !field[row][column].flag;
-      this.setState({field});
-    }
   }
 
   render() {
     const rows = [];
-    for (var i=0; i<this.state.field.length; i++) {
+    for (var i=0; i<this.props.field.length; i++) {
       var cells = [];
-      for (var j=0; j<this.state.field[i].length; j++) {
+      for (var j=0; j<this.props.field[i].length; j++) {
         cells.push(
           <MineCell
             key={j}
-            covered={this.state.field[i][j].covered}
-            flag={this.state.field[i][j].flag}
-            onContextMenu={this.flagCell.bind(this)}
-            onClick={this.uncoverCell.bind(this)}
+            covered={this.props.field[i][j].covered}
+            flag={this.props.field[i][j].flag}
+            onContextMenu={this.props.flagCell}
+            onClick={this.props.uncoverCell}
             row={i}
             column={j}
-            surrounding_mines={this.state.field[i][j].surrounding_mines}
+            surrounding_mines={this.props.field[i][j].surrounding_mines}
           />
         )
       }
--- src/index.jsx
+++ src/index.jsx
@@ -7,18 +7,124 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    const height = 8;
+    const width = 8;
+    const mine_count = 10;
+    const field = this.generateField(height, width, mine_count);
+
     this.state = {
-      height: 8,
-      width: 8,
-      mine_count: 10
+      height,
+      width,
+      mine_count,
+      field
+    };
+  }
+
+  generateField(height, width, mines) {
+    // Generate initial, empty field
+    var field = [];
+    for (var i=0; i<height; i++) {
+      var cells = []
+      for (var j=0; j<width; j++) {
+        cells.push({
+          covered: true,
+          flag: false,
+          surrounding_mines: 0
+        })
+      }
+      field.push(cells);
     }
+
+    // Add mines
+    for (var i=0; i<mines; i++) {
+      var row = Math.floor(Math.random() * height);
+      var column = Math.floor(Math.random() * width);
+      if ( field[row][column].surrounding_mines == 0) {
+        field[row][column].surrounding_mines = 9;
+      } else {
+        i--;
+      }
+    }
+
+    // Set surrounding mine counts
+    for (var i=0; i<height; i++) {
+      for (var j=0; j<width; j++) {
+        if ( field[i][j].surrounding_mines == 9 ) {
+          continue;
+        }
+
+        this.getSurroundingCells(i, j, height, width).forEach((item) => {
+          if (field[item[0]][item[1]].surrounding_mines == 9) {
+            field[i][j].surrounding_mines++;
+          }
+        });
+      }
+    }
+
+    return field;
+  }
+
+  getSurroundingCells(row, column, height, width) {
+    const cells = [];
+    [
+      [row-1, column-1],
+      [row-1, column],
+      [row-1, column+1],
+      [row, column-1],
+      [row, column+1],
+      [row+1, column-1],
+      [row+1, column],
+      [row+1, column+1]
+    ].forEach((cell) => {
+      if (cell[0] >= 0 && cell[1] >= 0 && cell[0] < height && cell[1] < width) {
+        cells.push([cell[0], cell[1]]);
+      }
+    });
+    return cells;
+  }
+
+  uncoverCell (row, column) {
+    const field = this.state.field;
+    if ( field[row][column].covered && !field[row][column].flag ) {
+      field[row][column].covered = false;
+      if ( field[row][column].surrounding_mines == 0 ) {
+        this.getSurroundingCells(row, column, this.state.height, this.state.width).forEach((cell) => {
+          if ( field[cell[0]][cell[1]].covered ) {
+            this.uncoverCell(cell[0], cell[1]);
+          }
+        });
+      }
+      this.setState({field: field});
+    }
+  }
+
+  flagCell (e, row, column) {
+    e.preventDefault();
+    const field = this.state.field;
+    if ( field[row][column].covered ) {
+      field[row][column].flag = !field[row][column].flag;
+      this.setState({field: field});
+    }
+  }
+
+  restart() {
+    this.setState({
+      field: this.generateField(this.state.height, this.state.width, this.state.mine_count)
+    });
   }
 
   render() {
     return (
       <div>
-        <h1>Mines</h1>
-        <MineField height={this.state.height} width={this.state.width} mines={this.state.mine_count} />
+        <h1 onClick={this.restart.bind(this)}>Mines</h1>
+        <MineField
+          height={this.state.height}
+          width={this.state.width}
+          mines={this.state.mine_count}
+          field={this.state.field}
+          uncoverCell={this.uncoverCell.bind(this)}
+          flagCell={this.flagCell.bind(this)}
+        />
       </div>
     );
   }
